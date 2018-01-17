01fac5e134a3455e2fce15e579f1edbb38b5da40
Joshua Gomez <joshuagomez@Joshuas-MacBook-Pro.local>
2018-01-16 19:06:44 -0500

    made connections

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('All American Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
